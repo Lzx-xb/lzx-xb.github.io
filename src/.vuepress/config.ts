2f54ffc903c9abcb116567614bc70ebc876db3b0
lzx-xb <321337450@qq.com>
2023-01-10 02:38:59 +0800

    update search config

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,9 +1,33 @@
 import { defineUserConfig } from "vuepress";
+import { docsearchPlugin } from '@vuepress/plugin-docsearch';
 import theme from "./theme.js";
 
 export default defineUserConfig({
   base: "/",
-
+  plugins: [
+    docsearchPlugin({
+      apiKey: 'f85ad85e4f4f2e660f17a66ef7c05648',
+      indexName: 'dev_blogSearch',
+      locales: {
+        '/': {
+          placeholder: 'Search Documentation',
+          translations: {
+            button: {
+              buttonText: 'Search Documentation',
+            },
+          },
+        },
+        '/zh/': {
+          placeholder: '搜索文档',
+          translations: {
+            button: {
+              buttonText: '搜索文档',
+            },
+          },
+        },
+      },
+    }),
+  ],
   locales: {
     "/": {
       lang: "en-US",
